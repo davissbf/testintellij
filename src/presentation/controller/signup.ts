ac2912c88efc8cfeb3cfa4bb140fc4a55f2eb95b
davi <davissbf@gmail.com>
2021-06-27 19:04:40 -0300

    validando condição para falta de parâmento nome e email

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -1,8 +1,16 @@
 export class SignUpController {
   handle (httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error ('Esperado parâmetro: nome'),
-    };
+    if (!httpRequest.body.nome) {
+      return {
+        statusCode: 400,
+        body: new Error ('Esperado parâmetro: nome'),
+      }
+    }
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error ('Esperado parâmetro: email'),
+      }
+    }
   }
 }
