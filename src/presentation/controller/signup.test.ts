c576505b10e4bdd231eea3f23757089ec91eb1a9
davi <davissbf@gmail.com>
2021-06-27 19:05:17 -0300

    corrigindo a falta do body dentro de httpRequest

--- src/presentation/controller/signup.test.ts
+++ src/presentation/controller/signup.test.ts
@@ -4,11 +4,28 @@ describe('SignUp Controller', () => {
   test('Retornar erro 400, se o nome não for enviado', () => {
     const sut = new SignUpController();
     const httpRequest = {
-      email: 'any_email@email.com',
-      password: 'any_password',
-      passwordConfirmation: 'any_password',
+      body: {
+        email: 'any_email@email.com',
+        senha: 'any_password',
+        confirmarSenha: 'any_password',
+      }
     }
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
-  })
-})
+    expect(httpResponse.body).toEqual(new Error ('Esperado parâmetro: nome'));
+  });
+});
+
+test('Retornar erro 400, se o email não for enviado', () => {
+  const sut = new SignUpController();
+  const httpRequest = {
+    body: {
+      nome: 'any_nome',
+      senha: 'any_password',
+      confirmarSenha: 'any_password',
+    }
+  }
+  const httpResponse = sut.handle(httpRequest);
+  expect(httpResponse.statusCode).toBe(400);
+  expect(httpResponse.body).toEqual(new Error ('Esperado parâmetro: email'));
+});
