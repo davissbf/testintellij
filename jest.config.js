7549688e561d5e6569dba94bce96d6528d7a6673
davi <davissbf@gmail.com>
2021-06-27 16:25:27 -0300

    Removendo pastas e arquivos e configurando jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
-export default {
+module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/test/src/**/*.ts'],
   coverageDirectory: "coverage",
   testEnvironment: "node",
   transform: {
