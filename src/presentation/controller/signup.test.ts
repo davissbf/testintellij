887467b1081a1705a41a6a772c9d723c46ce951c
davi <davissbf@gmail.com>
2021-06-28 16:58:45 -0300

    error personalizado para email invaido

--- src/presentation/controller/signup.test.ts
+++ src/presentation/controller/signup.test.ts
@@ -1,20 +1,39 @@
 import { SignUpController } from "./signup";
 import { ErrorParametroAusente } from '../errors/error-parametro-ausente';
+import { ErrorParametroInvalido } from '../errors/error-parametro-invalido';
+import { ValidadorEmail } from '../protocols/validador-email';
 
-const makeSut = (): SignUpController => {
-  return new SignUpController();
+interface SutTypes {
+  sut: SignUpController;
+  validadorEmailStub: ValidadorEmail;
+}
+
+const makeSut = (): SutTypes => {
+  class ValidadorEmailStub implements ValidadorEmail {
+    emailValido(email: string): boolean {
+      return true;
+    }
+  }
+
+  const validadorEmailStub = new ValidadorEmailStub();
+  const sut = new SignUpController(validadorEmailStub);
+  return {
+    sut,
+    validadorEmailStub,
+  }
 }
 
 describe('SignUp Controller', () => {
   test('Retornar erro 400, se o nome não for enviado', () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequisicao = {
       body: {
-        email: 'any_email@email.com',
-        senha: 'any_password',
-        confirmarSenha: 'any_password',
+        email: 'qualquer_email@email.com',
+        senha: 'qualquer_password',
+        confirmarSenha: 'qualquer_password',
       }
     }
+
     const httpResposta = sut.handle(httpRequisicao);
     expect(httpResposta.statusCode).toBe(400);
     expect(httpResposta.body).toEqual(new ErrorParametroAusente('nome'));
@@ -22,43 +41,65 @@ describe('SignUp Controller', () => {
 });
 
 test('Retornar erro 400, se o email não for enviado', () => {
-  const sut = makeSut();
+  const { sut } = makeSut();
   const httpRequisicao = {
     body: {
-      nome: 'any_nome',
-      senha: 'any_password',
-      confirmarSenha: 'any_password',
+      nome: 'qualquer_nome',
+      senha: 'qualquer_password',
+      confirmarSenha: 'qualquer_password',
     }
   }
+
   const httpResposta = sut.handle(httpRequisicao);
   expect(httpResposta.statusCode).toBe(400);
   expect(httpResposta.body).toEqual(new ErrorParametroAusente('email'));
 });
 
 test('Retornar erro 400, se a senha não for enviada', () => {
-  const sut = makeSut();
+  const { sut } = makeSut();
   const httpRequisicao = {
     body: {
-      nome: 'any_nome',
-      email: 'any_email@email.com',
-      confirmarSenha: 'any_password',
+      nome: 'qualquer_nome',
+      email: 'qualquer_email@email.com',
+      confirmarSenha: 'qualquer_password',
     }
   }
+
   const httpResposta = sut.handle(httpRequisicao);
   expect(httpResposta.statusCode).toBe(400);
   expect(httpResposta.body).toEqual(new ErrorParametroAusente('senha'));
 });
 
 test('Retornar erro 400, se o consfirmar senha não for enviado', () => {
-  const sut = makeSut();
+  const { sut } = makeSut();
   const httpRequisicao = {
     body: {
-      nome: 'any_nome',
-      email: 'any_email@email.com',
-      senha: 'any_password',
+      nome: 'qualquer_nome',
+      email: 'qualquer_email@email.com',
+      senha: 'qualquer_password',
     }
   }
+
   const httpResposta = sut.handle(httpRequisicao);
   expect(httpResposta.statusCode).toBe(400);
   expect(httpResposta.body).toEqual(new ErrorParametroAusente('confirmarSenha'));
+});
+
+test('Retornar erro 400, se o email não for válido', () => {
+  const { sut, validadorEmailStub } = makeSut();
+
+  jest.spyOn(validadorEmailStub, 'emailValido').mockReturnValueOnce(false);
+
+  const httpRequisicao = {
+    body: {
+      nome: 'qualquer_nome',
+      email: 'ivalido_email@email.com',
+      senha: 'qualquer_password',
+      confirmarSenha: 'qualquer_password',
+    }
+  }
+
+  const httpResposta = sut.handle(httpRequisicao);
+  expect(httpResposta.statusCode).toBe(400);
+  expect(httpResposta.body).toEqual(new ErrorParametroInvalido('email'));
 });
