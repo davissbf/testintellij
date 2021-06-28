ecd11eab679f1753d022a6ce8aaeb2f59d76c7e0
davi <davissbf@gmail.com>
2021-06-28 00:02:28 -0300

    Refatorando código duplicado para método helper

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -1,19 +1,14 @@
 import { HttpResponse, HttpRequest } from '../protocols/http';
 import { ErrorParametroAusente } from '../errors/error-parametro-ausente';
+import { requisicaoRuim } from '../helpers/http-helpers';
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.nome) {
-      return {
-        statusCode: 400,
-        body: new ErrorParametroAusente('nome'),
-      }
+      return requisicaoRuim(new ErrorParametroAusente('nome'));
     }
     if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new ErrorParametroAusente('email'),
-      }
+      return requisicaoRuim(new ErrorParametroAusente('email'));
     }
   }
 }
